44e579ed4a256025d73fa1f633ea9adc288060f2
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-06-01 16:49:37 +0300

    screen fix-12

--- wp-content/themes/prosper/js/script.js
+++ wp-content/themes/prosper/js/script.js
@@ -3,6 +3,7 @@ jQuery(function() {
     videoResizer = function(){
       screen = jQuery(window).width()/3.1
       jQuery('.slider video').css('height', screen+'px');
+      console.log(screen);
     }
   if ('.slider') {
     videoResizer();
