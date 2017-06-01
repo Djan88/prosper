3ceee22ad212c08448526312c507251920117890
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-06-01 16:51:35 +0300

    screen fix-13

--- wp-content/themes/prosper/js/script.js
+++ wp-content/themes/prosper/js/script.js
@@ -5,10 +5,8 @@ jQuery(function() {
       jQuery('.slider video').css('height', screen+'px');
       console.log(screen);
     }
-  if ('.slider') {
+  videoResizer();
+  jQuery(window).resize(function() {
     videoResizer();
-    jQuery(window).resize(function() {
-      videoResizer();
-    }
   }
 });
