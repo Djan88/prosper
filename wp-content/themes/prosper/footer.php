be421bd4a5b45d0ba35bcf397904436eb39711fb
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-06-01 16:48:29 +0300

    screen fix-11

--- wp-content/themes/prosper/footer.php
+++ wp-content/themes/prosper/footer.php
@@ -64,6 +64,7 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/slick.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
     <script>
         $(document).ready(function(){
             $('.slider__container').slick({
---
+++ wp-content/themes/prosper/js/script.js
@@ -0,0 +1,13 @@
+jQuery(function() {
+  var screen,
+    videoResizer = function(){
+      screen = jQuery(window).width()/3.1
+      jQuery('.slider video').css('height', screen+'px');
+    }
+  if ('.slider') {
+    videoResizer();
+    jQuery(window).resize(function() {
+      videoResizer();
+    }
+  }
+});
