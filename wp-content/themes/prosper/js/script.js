bb069093688ad790b13f4771c0ad7cd3265850b5
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-06-03 10:32:58 +0300

    screen fix-23

--- wp-content/themes/prosper/js/script.js
+++ wp-content/themes/prosper/js/script.js
@@ -1,11 +1,14 @@
 jQuery(function() {
   var screen,
       stripe_w,
+      stripe_pb,
     videoResizer = function(){
       screen = jQuery(window).width()/3.1;
       stripe_w = jQuery(window).width()/51.2;
+      stripe_w = jQuery(window).width()/52.2;
       jQuery('.slider video').css('height', screen+'px');
       jQuery('.video_stripe').css('height', stripe_w+'px');
+      jQuery('.video_stripe').css('paddingBottom', stripe_pb+'px');
       console.log(screen);
     }
   if ('.slider') {
