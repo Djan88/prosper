88c767bd043635df67b446d59827ec1de7492972
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-06-03 10:49:46 +0300

    screen fix-30

--- wp-content/themes/prosper/js/script.js
+++ wp-content/themes/prosper/js/script.js
@@ -5,7 +5,7 @@ jQuery(function() {
     videoResizer = function(){
       screen = jQuery(window).width()/3.1;
       stripe_w = jQuery(window).width()/51.2;
-      stripe_pb = jQuery(window).width()/52;
+      stripe_pb = jQuery(window).width()/51.5;
       jQuery('.slider video').css('height', screen+'px');
       jQuery('.video_stripe').css('height', stripe_w+'px');
       jQuery('.slider video').css('padding-bottom', stripe_pb+'px');
